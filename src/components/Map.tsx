0c6892208dc814e8e3f970df0836b10bbb66501e
Fanny Hoberg <fannyh.92@hotmail.com>
2024-12-06 13:40:44 +0100

    added info window to show when clicking on map marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,44 @@
-import { GoogleMap, Marker } from "@react-google-maps/api";
-import { PositionCoords } from "../types/trip";
-import { useCallback, useMemo } from "react";
+import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+import { Item, PositionCoords } from "../types/trip";
+import { useCallback, useMemo, useState } from "react";
 import useGetTrip from "../hooks/useGetTrip";
 import { useParams } from "react-router-dom";
+import { GeoPoint } from "firebase/firestore";
+import { Box, Typography } from "@mui/material";
 
 const Map = () => {
-  const defaultCenter: PositionCoords = { lat: 37.7749, lng: -122.4194 };
-
   const { id } = useParams();
   const { data: trip, isError, isLoading } = useGetTrip(id);
+  const [selectedItem, setSelectedItem] = useState<null | {
+    title: string;
+    adress: string;
+    city: string;
+    position: PositionCoords;
+  }>(null);
 
   const onLoad = useCallback((mapInstance: google.maps.Map) => {
     console.log("Map loaded", mapInstance);
   }, []);
+
+  const markerIcon = `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(`
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40">
+      <circle cx="12" cy="12" r="11" fill="#FF7D70" />
+    </svg>
+  `)}`;
+
+  const handleMarkerClick = (item: Item, position: GeoPoint) => {
+    setSelectedItem({
+      title: item.title,
+      adress: item.address,
+      city: item.city,
+      position: {
+        lat: position.latitude,
+        lng: position.longitude,
+      },
+    });
+  };
+
+  const defaultCenter: PositionCoords = { lat: 37.7749, lng: -122.4194 };
 
   const center = useMemo(() => {
     if (!trip) return defaultCenter;
@@ -72,22 +98,36 @@ const Map = () => {
           return list?.items?.map((item) => {
             const { geopoint } = item;
 
-            // Om geopoint finns, skapa en marker
             if (geopoint) {
               return (
                 <Marker
                   key={item._id}
+                  onClick={() => handleMarkerClick(item, geopoint)}
                   position={{
                     lat: geopoint.latitude,
                     lng: geopoint.longitude,
                   }}
                   title={item.title}
+                  icon={markerIcon}
                 />
               );
             }
             return null;
           });
         })}
+
+        {selectedItem && (
+          <InfoWindow
+            position={selectedItem.position}
+            onCloseClick={() => setSelectedItem(null)}
+          >
+            <Box>
+              <Typography variant="h5">{selectedItem.title}</Typography>
+              <Typography>{selectedItem.adress}</Typography>
+              <Typography>{selectedItem.city}</Typography>
+            </Box>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </div>
   );
