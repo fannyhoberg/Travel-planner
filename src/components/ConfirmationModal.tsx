b39479065479b6aa1df1880daa87f404032065c4
Fanny Hoberg <fannyh.92@hotmail.com>
2024-12-11 11:05:59 +0100

    changed styling on confirmation modal

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -23,8 +23,8 @@ const ConfirmationModal = ({
     transform: "translate(-50%, -50%)",
     width: 400,
     bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
+    border: "0.5px solid lightgray",
+    boxShadow: 5,
     p: 4,
   };
   return (
