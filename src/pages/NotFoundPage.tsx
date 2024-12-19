920d7cfa8e78ee28f6603f968aef6f4a89bd0967
Fanny Hoberg <fannyh.92@hotmail.com>
2024-12-19 12:55:58 +0100

    added image to not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,48 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import NotFoundMobile from "../assets/images/404-not-found_mobile.png";
+import NotFoundDesktop from "../assets/images/404-not-found_desktop.png";
+
 const NotFoundPage = () => {
-  return <div>Page not found...</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        mt: 5,
+        overflow: "hidden",
+      }}
+    >
+      <Typography variant="h2" sx={{ mb: 4 }}>
+        Looks like you're lost?
+      </Typography>
+
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <img
+          src={isMobile ? NotFoundMobile : NotFoundDesktop}
+          alt="404 Not Found"
+          style={{
+            width: isMobile ? "100%" : "50%",
+            height: isMobile ? "100%" : "50%",
+            maxHeight: "100vh",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default NotFoundPage;
